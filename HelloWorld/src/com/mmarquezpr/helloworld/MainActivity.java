5d0e3500fb2408654d1bbc85ff7653afe1341969
mmarquezpr <memarquez01@gmail.com>
2014-03-11 16:45:51 -0400

    First Commit, and HelloWorld Demo Inclusion

---
+++ HelloWorld/src/com/mmarquezpr/helloworld/MainActivity.java
@@ -0,0 +1,29 @@
+package com.mmarquezpr.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.getActionBar().hide();
+        
+        TextView tv = (TextView) findViewById(R.id.tv_hello_world);
+        tv.setText("Hello, Hack PR!");
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
